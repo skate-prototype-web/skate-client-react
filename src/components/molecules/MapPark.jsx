0428916dfdbe8ed741b5e3ce8c51517e872169fe
Scott Nassau <snassau@gmail.com>
2020-05-22 22:32:57 -0700

    update responsive design

--- src/components/molecules/MapPark.jsx
+++ src/components/molecules/MapPark.jsx
@@ -8,32 +8,51 @@ const MapContainer = styled.div`
   border: solid rgb(119, 33, 46);
   margin-top: 3vw; 
   width: 85vw;
-  height: 50vw; 
+  height: 50vw;
+  @media(min-width: 1100px) {
+    width: 860px; 
+    height: 550px; 
+    margin-top: 35px; 
+  } 
 `
 
 const SkateImage = styled.img `
-  height: 4.5vw; 
+  height: 2.5vw; 
   width: auto;
   
   &:hover{
     cursor: pointer;
     height: 3.25vw;  
   }
+
+  @media (min-width: 1100px) {
+    height: 30px;
+    width: auto;
+  }
 `
 
 const PopupName = styled.h3`
   font-family: 'Dosis', sans-serif;
   font-weight: 400; 
-  font-size: 2.25vw;  
+  font-size: 2.25vw;
+  
+  @media(min-width: 1100px){
+    font-size: 24px;
+  }
 `
+
 const PopupDetails = styled.p`
   font-family: 'Dosis', sans-serif; 
   font-weight: 200;
-  font-size: 2vw;   
+  font-size: 2vw;
+  
+  @media(min-width: 1100px) {
+    font-size: 20px; 
+  }
 `
 
 const PopupContainer = styled.div`
-  width: 300px;  
+  width: 300px; 
 `
 
 const MapPark = props => {
@@ -44,8 +63,8 @@ const MapPark = props => {
   const viewIt = {latitude: pinLat,
                 longitude: pinLong,
                 zoom: 15,
-                width: '85vw',
-                height: '50vw'
+                width: '100%',
+                height: '100%'
               }
               
   const [viewport, setViewport] = useState (viewIt)
