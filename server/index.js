02a29eb2244152d82783d3efe5eb0d874b6e7276
Scott Nassau <snassau@gmail.com>
2020-06-22 22:41:49 -0700

    add route for merch

--- server/index.js
+++ server/index.js
@@ -13,6 +13,7 @@ app.use(express.static(path.join(__dirname, '../dist')));
 
 app.use('/about', express.static(path.join(__dirname, '../dist')))
 app.use('/skateparks', express.static(path.join(__dirname, '../dist')))
+app.use('/merch', express.static(path.join(__dirname, '../dist')))
 app.use ('/skateparks/:id', express.static(path.join(__dirname, '../dist')))
 
 app.listen(port, () => console.log(`connected to port ${port}!`))
